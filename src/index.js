42fe698f5fa2aea1ab521c2a2be07bb99f1fabe8
DamilolaE <demiola.oe@gmail.com>
2024-09-16 19:11:38 +0100

    update more js, general tidying up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 function updateWeather(response) {
   let tempElement = document.querySelector("#temp-value");
-  tempElement.innerHTML = Math.round(response.data.temperature.current);
   let windElement = document.querySelector("#weather-windspeed");
-  windElement.innerHTML = response.data.wind.speed;
   let humidityElement = document.querySelector("#weather-humidity");
-  humidityElement.innerHTML = response.data.temperature.humidity;
   let descriptionElement = document.querySelector("#weather-description");
-  descriptionElement.innerHTML = response.data.condition.description;
   let cityElement = document.querySelector("#city");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#temp-icon");
+
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = timeNow(date);
+  iconElement.src = response.data.condition.icon_url;
+}
+
+function timeNow(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
 }
 
 function fetchCityDetails(city) {
@@ -24,5 +49,7 @@ function submitCityValue(event) {
   fetchCityDetails(searchInput.value);
 }
 
+let defaultApiUrl = `https://api.shecodes.io/weather/v1/current?query=Lagos&key=52b8211ebf3ae3d722a0780tdeof04f0&units=metric`;
+axios.get(defaultApiUrl).then(updateWeather);
 let formElement = document.querySelector("#enter-city");
 formElement.addEventListener("submit", submitCityValue);
